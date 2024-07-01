2545c40c0be56a9d734f8daffac97b01cf957b8d
Log-Wall <mrwallacelk@gmail.com>
2024-06-30 18:57:45 -0700

    Deploy website - based on f4219c4a4fd5cf6142baee84577095f7c6b84074

---
+++ assets/js/01104bdf.18c9da6c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknexfiles=self.webpackChunknexfiles||[]).push([[2669],{2420:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/nex-files/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.5c7da90f.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunknexfiles=self.webpackChunknexfiles||[]).push([[1903],{1912:e=>{e.exports=JSON.parse('{"blogBasePath":"/nex-files/blog","blogTitle":"Blog"}')}}]);
